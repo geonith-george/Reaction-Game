5ba7dc3d6917ffafc0eff34bc7992d5efb906a3c
geonith-george <geonith98@gmail.com>
2020-03-11 15:38:17 +0530

    cookie's added

--- app.js
+++ app.js
@@ -48,11 +48,21 @@ function start() {
     started = setInterval(clockRunning, 10);	
 }
 
+
 function stop() {
     timeStopped = new Date();
+    c_score = document.getElementById("n").innerText;
     clearInterval(started);
     document.getElementById("start").disabled = true;
     document.getElementById("click").disabled = true;
+
+    console.log(parseFloat(c_score) < parseFloat(getCookie("currentscore")));
+
+    if( parseFloat(c_score) < parseFloat(getCookie("currentscore"))){
+        setCookie("currentscore", c_score, 30);
+        document.getElementById("highscore").innerText="High-Score: "+getCookie("currentscore"); 
+    }
+
 }
  
 function reset() {
@@ -74,6 +84,41 @@ function clockRunning(){
         , ms = timeElapsed.getUTCMilliseconds();
 
     document.getElementById("n").innerHTML =
-        (sec > 9 ? sec : "0" + sec) + ":" + 
+        (sec > 9 ? sec : "0" + sec) + "." + 
         (ms > 99 ? ms : ms > 9 ? "0" + ms : "00" + ms);
 };
+
+// cookie
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+  function checkCookie() {
+    var score=getCookie("currentscore");
+    console.log(score);
+    if (score==NaN) 
+    {
+    setCookie("currentscore", 10 , 30); 
+    console.log("cookie set to 10");
+    }
+    document.getElementById("highscore").innerText="High-Score: "+getCookie("currentscore"); 
+  }
